4321a5970e41280a41e92178c72392fa20a60ff4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-01-15 15:34:34 +0100

    align with upstream

--- flasher_stub/stub_io.c
+++ flasher_stub/stub_io.c
@@ -171,12 +171,12 @@ void stub_tx_one_char(char c)
 #endif // WITH_USB_OTG
   uart_tx_one_char(c);
 #if WITH_USB_JTAG_SERIAL
-  static int transferred_without_flush = 0;
+  static unsigned short transferred_without_flush = 0;
   if (stub_uses_usb_jtag_serial()){
-    // Defer flushing until we have a (full - 1) packet or a 0xc0 byte to increase throughput.
-    // Note that deferring flushing until we have a full packet causes hang-ups on some platforms.
-    transferred_without_flush++;
-    if( c == '\xc0' || transferred_without_flush == 63 ) {
+    // Defer flushing until we have a (full - 1) packet or a end of packet (0xc0) byte to increase throughput.
+    // Note that deferring flushing until we have a full packet can cause hang-ups on some platforms.
+    ++transferred_without_flush;
+    if (c == '\xc0' || transferred_without_flush >= 63) {
       stub_tx_flush();
       transferred_without_flush = 0;
     }
@@ -303,15 +303,6 @@ void stub_io_set_baudrate(uint32_t current_baud, uint32_t new_baud)
     return;
   }
 #endif // WITH_USB_OTG
-#if WITH_USB_JTAG_SERIAL
-  /* Workaround for ESP32-S3: UART baud rate divider is not set correctly by get_new_uart_divider. */
-  if (stub_uses_usb_jtag_serial()) {
-    ets_delay_us(10000);
-    uart_div_modify(0, 0);
-    ets_delay_us(1000);
-    return;
-  }
-#endif
   ets_delay_us(10000);
   uart_div_modify(0, get_new_uart_divider(current_baud, new_baud));
   ets_delay_us(1000);
